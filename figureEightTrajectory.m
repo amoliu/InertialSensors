1e93dc3b9859d1978ccc04d61710bd5ae25ebc5c
Will Conlin <wtconlin@gmail.com>
2016-11-01 10:57:21 -0500

    Update figureEightTrajectory.m

--- figureEightTrajectory.m
+++ figureEightTrajectory.m
@@ -1,12 +1,9 @@
-%Define T, t
-T = 60; %seconds
-t = 0:1:30;
+% 8-curve
+T = 60;
+t = 0:0.1:60;
+x = 4*sin(2*pi/T*t);
+y = 4*sin(4*pi/T*t);
+z = cos(4*pi/T*t);
 
-%Calculate the position
-x=4*sind(2*pi*T*t);
-y=4*sind(4*pi*T*t);
-z=cosd(4*pi*T*t);
-
-%Plot
-figure
 plot3(x,y,z);
+grid on;
