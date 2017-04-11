9eff55f3903448b8c1445474aa4fd25844a69022
Will Conlin <wtconlin@gmail.com>
2017-04-11 17:38:06 -0500

    Create ComplementaryFilter.m

---
+++ ComplementaryFilter.m
@@ -0,0 +1,4 @@
+% Will Conlin, 4/11/17
+% Madgwick Complementary Filter
+% Inputs: True virtual fish trajectory data, simulated trajectory data
+% Models: State dynamics, error, complementary filter
