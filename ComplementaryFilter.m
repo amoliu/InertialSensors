ceddfe4fa71077e28fc8c77e2b76298fd90758f0
Will Conlin <wtconlin@gmail.com>
2017-04-11 18:21:07 -0500

    Update ComplementaryFilter.m

--- ComplementaryFilter.m
+++ ComplementaryFilter.m
@@ -2,3 +2,28 @@
 % Madgwick Complementary Filter
 % Inputs: True virtual fish trajectory data, simulated trajectory data
 % Models: State dynamics, error, complementary filter
+
+function [results] = ComplementaryFilter(simimu,varargin)
+% simimu.Qgyro
+% simimu.Qacc
+% simimu.Qbias
+% simimu.realeulerrad
+% simimu.dynaccGlobal
+% simimu.gyro
+% simimu.acc
+% simimu.t
+% simimu.sampfreq
+% simimu.gyronoisestd
+% simimu.gyrobiasdriftstd
+% simimu.accnoisestd
+
+    % Setup
+    
+    % Method
+        % angle = gamma * (angle + gyroData * dt) + (1-gamma) * accelData
+        % infinite loop
+        % vary gamma
+        
+        
+    results = simimu.t; % Placeholder
+end
