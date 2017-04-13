037a3e8729a571455da90d245f9d4531a5c1794e
Will Conlin <wtconlin@gmail.com>
2017-04-13 11:32:39 -0500

    Update ComplementaryFilter.m

--- ComplementaryFilter.m
+++ ComplementaryFilter.m
@@ -5,7 +5,7 @@
 % Instructions: A call would look like:
 %   ComplementaryFilter(simulatedData(5,1)) where the 1 is to close plots
 
-function [results] = ComplementaryFilter(simimu,varargin)
+function [simimu] = ComplementaryFilter(simimu,varargin)
 % simimu.Qgyro
 % simimu.Qacc
 % simimu.Qbias
@@ -28,18 +28,18 @@ time = simimu.t;
         % angle = gamma * (angle + gyroData * dt) + (1-gamma) * accelData
         
         % vary gamma
-gamma = .98;
+gamma = .1;
 yaw = zeros(size(time));
-yawTemp = 0;
+yawGyro = 0;
 
 for ii = 1:size(time)
-    yawTemp = yawTemp + simimu.gyro(ii, 3) * simimu.sampfreq; %*pitch += ((float)gyrData[0] / GYROSCOPE_SENSITIVITY) * dt; % Angle around the X-axis
+    yawGyro = yawGyro + simimu.gyro(ii, 3) * simimu.sampfreq; % current angle + rate * dt
     
-    if simimu.acc(ii, 2) > .1  % y acc threshhold
-        yawAcc = atan(simimu.acc(ii, 2)) * 180 / pi;
-        yaw(ii) = yawTemp * gamma + yawAcc * (1-gamma);
+    if abs(simimu.acc(ii, 2)) < .1  % y acc threshhold
+        yaw(ii) = accToAngle(simimu.acc(ii, 1), simimu.acc(ii, 2), simimu.acc(ii, 3) ) * gamma + yawGyro * (1-gamma);
+        yawGyro = yaw(ii);
     else
-        yaw(ii) = yawTemp;
+        yaw(ii) = yawGyro;
     end
 end
     % Plot
@@ -47,33 +47,41 @@ f = figure('Name','Pitch vs. Time'); %New fig
 set(f, 'Position', [100, 100, 1049, 895]);
 
 subplot(2,2,1);
-plot(time, simimu.truegyro(:,3));
+plot(time, rad2deg(simimu.truegyro(:,3)));
 title('Yaw');
 legend('IMU Data')
 xlabel('time (seconds)'); ylabel('degrees/sec');
 
 subplot(2,2,2);
+plot(time, (rad2deg(simimu.truegyro(:,3)) - rad2deg(simimu.gyro(:,3))));
+title('Yaw Gyro Error');
+legend('Error')
+xlabel('time (seconds)'); ylabel('degrees/sec');
+
+subplot(2,2,3);
 plot(time, rad2deg(yaw));
 title('Complementary Filter Yaw');
 legend('Complementary Filter')
 xlabel('time (seconds)'); ylabel('degrees/sec');
 
-subplot(2,2,3);
+subplot(2,2,4);
 plot(time, (rad2deg(simimu.truegyro(:,3)) - rad2deg(yaw)));
 title('Complementary Filter Yaw Gyro Error');
 legend('Error')
 xlabel('time (seconds)'); ylabel('degrees/sec');
 
-subplot(2,2,4);
-plot(time, (rad2deg(simimu.truegyro(:,3)) - simimu.gyro(:,3)));
-title('Yaw Gyro Error');
-legend('Error')
-xlabel('time (seconds)'); ylabel('degrees/sec');
 
 if(not(isempty(varargin)))
     if varargin{1}==1
         close all;
     end
 end
+
+end
+
+function yAngle = accToAngle(ax, ay, az)
+% Just for yAngle now
+   yAngle = atan( ay / (ax^2 + az^2)^.5) * 180 / pi;
+%    yAngle = atan( ax + az) * 180 / pi;
 
 end
