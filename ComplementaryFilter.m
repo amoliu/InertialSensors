2214714520e34252c0ce0b789268af03f1d4f0e6
Will Conlin <wtconlin@gmail.com>
2017-04-21 10:23:54 -0500

    Update ComplementaryFilter.m

--- ComplementaryFilter.m
+++ ComplementaryFilter.m
@@ -24,8 +24,8 @@ global simimulocal
     % Setup
         % Plot imu data (via simulatedData.m)
 time = simimu.t;
-window =2001;
-simimu.acc = movmean(simimu.acc * 9.81, window);
+% window =201;
+% simimu.acc = movmean(simimu.acc * 9.81, window);
 simimulocal = simimu;
 
     % Method
@@ -33,19 +33,19 @@ simimulocal = simimu;
             % if a is above a threshold, use gyro data
         % angle = gamma * (angle + gyroData * dt) + (1-gamma) * accelData
         
-        % vary gamma
+        % vary gamma (optimization below)
 gamma = .6; % .02
 pitch = zeros(size(time));
 pitchAccM = zeros(size(time));
+pitchGyro =  cumtrapz(simimu.gyro(:, 2)) * simimu.sampfreq;
 
 for ii = 1:size(time)
-    pitchGyro =  simimu.gyro(ii, 2) * simimu.sampfreq; % integrate
     if abs(simimu.acc(ii, 2)) < .981  % y acc threshhold
         pitchAcc = accToAngle(simimu.acc(ii, 1), simimu.acc(ii, 2), simimu.acc(ii, 3) ); 
-        pitch(ii) = pitchAcc.pitch * gamma + pitchGyro * (1-gamma);
+        pitch(ii) = pitchAcc.pitch * gamma + pitchGyro(ii) * (1-gamma);
         pitchAccM(ii) = pitchAcc.pitch;
     else
-        pitch(ii) = pitchGyro;
+        pitch(ii) = pitchGyro(ii);
         pitchAccM(ii) = 0;
     end
 end
@@ -54,25 +54,25 @@ f = figure('Name','Pitch vs. Time'); %New fig
 set(f, 'Position', [100, 100, 1049, 895]);
 
 subplot(2,2,1);
-plot(time, simimu.gyro(:,2) * simimu.sampfreq, time, simimu.truegyro(:,2) * simimu.sampfreq); % integral of omega     vel = cumtrapz(acc) * dT + v0
+plot(time, cumtrapz(simimu.gyro(:,2))* simimu.sampfreq, time, cumtrapz(simimu.truegyro(:,2)) * simimu.sampfreq); % integral of omega     vel = cumtrapz(acc) * dT + v0
 title('Pitch');
 legend('IMU Data')
 xlabel('time (seconds)'); ylabel('radians');
 
 subplot(2,2,2);
-plot(time, abs( (simimu.truegyro(:,2) - simimu.gyro(:,2)) * simimu.sampfreq));
+plot(time, abs( cumtrapz(simimu.truegyro(:,2) - simimu.gyro(:,2)) * simimu.sampfreq));
 title('Pitch Gyro Error (abs. val.)');
 legend('Error')
 xlabel('time (seconds)'); ylabel('radians');
 
 subplot(2,2,3);
-plot(time, pitch, time, simimu.truegyro(:,2) * simimu.sampfreq);
+plot(time, pitch, time, cumtrapz(simimu.truegyro(:,2)) * simimu.sampfreq);
 title('Complementary Filter Pitch');
 legend('Complementary Filter')
 xlabel('time (seconds)'); ylabel('radians');
 
 subplot(2,2,4);
-plot(time, abs( (simimu.truegyro(:,2) * simimu.sampfreq) - pitch ));
+plot(time, abs( cumtrapz(simimu.truegyro(:,2)) * simimu.sampfreq - pitch ));
 title('Complementary Filter Pitch Error (abs. val.)');
 legend('Error')
 xlabel('time (seconds)'); ylabel('radians');
